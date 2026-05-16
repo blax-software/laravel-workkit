8bb3e6499a47c502e42ce6da8c7bf38d7915446d
Fabian Wagner <fabian@blax.at>
2026-05-16 11:21:32 +0200

    A middlewares

---
+++ src/Middleware/RequireAuthMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Blax\Workkit\Middleware;
+
+use Blax\Workkit\Services\MiscService;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequireAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $action = 'continue'): Response
+    {
+        if (! Auth::check()) {
+            return response()->json(
+                MiscService::apiResponse(['message' => "You need to be logged in to {$action}."]),
+                Response::HTTP_UNAUTHORIZED,
+            );
+        }
+
+        return $next($request);
+    }
+}
